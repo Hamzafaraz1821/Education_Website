33897742267c44c529cfac2b422f026ab6d6c58c
Hamza Faraz <124732544+Hamzafaraz1821@users.noreply.github.com>
2023-08-27 15:56:00 +0500

    Project 2

---
+++ js/script.js
@@ -0,0 +1,50 @@
+// chage navbar style on scroll
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0);
+})
+
+// show/hid faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+        //change icon
+        const icon = faq.querySelector('.faq__icon img');
+
+        if (icon.src.endsWith('plus.svg')) {
+            icon.src = 'assets/minus.svg';
+        } else {
+            icon.src = 'assets/plus.svg';
+        }
+    })
+})
+
+
+// show/hide nav menu
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("#open-menu-btn");
+const closeBtn = document.querySelector("#close-menu-btn");
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+})
+
+
+// close nav menu
+
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
+
+
